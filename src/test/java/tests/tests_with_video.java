895af1539c68ffcffe442c9c0b1aa67537f20d98
soclosetowin <sdiliara@yandex.ru>
2020-09-23 17:03:06 +0300

    back to google url

--- src/test/java/tests/tests_with_video.java
+++ src/test/java/tests/tests_with_video.java
@@ -15,7 +15,7 @@ public class tests_with_video {
     @Test
     void easy_selenide_test_google() {
         step("Открыть гугл", ()-> {
-            open("https://en.wikipedia.org/wiki/Main_Page");
+            open("https://www.google.com/");
         });
         step("Выполнить запрос", ()-> {
             $("[name=\"q\"]").setValue("wikipedia").pressEnter();
