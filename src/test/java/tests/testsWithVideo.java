76776ea28ef066f9e48be3cf43a21262503b03b8
soclosetowin <sdiliara@yandex.ru>
2020-09-23 17:30:33 +0300

    some fix

--- src/test/java/tests/testsWithVideo.java
+++ src/test/java/tests/testsWithVideo.java
@@ -1,19 +1,16 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
-import static org.junit.Assert.assertThat;
 
-public class tests_with_video {
-    
+public class testsWithVideo {
+
     @Test
-    public void easy_selenide_test_google() {
+    void easy_selenide_test_google() {
         step("Открыть гугл", ()-> {
             open("https://www.google.com/");
         });
