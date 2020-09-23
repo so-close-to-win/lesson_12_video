0c65f60df4a3879bcb6e66cc6fe1d806c09d0cec
soclosetowin <sdiliara@yandex.ru>
2020-09-23 16:37:46 +0300

    New google link

--- src/test/java/tests/tests_with_video.java
+++ src/test/java/tests/tests_with_video.java
@@ -15,7 +15,7 @@ public class tests_with_video {
     @Test
     void easy_selenide_test_google() {
         step("Открыть гугл", ()-> {
-            open("https://www.google.ru/");
+            open("https://www.google.com/");
         });
         step("Выполнить запрос", ()-> {
             $("[name=\"q\"]").setValue("wikipedia").pressEnter();
